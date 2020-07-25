1125562da9bdb9aa46eeb29c71e2e4b8902f2205
Pa-Ka <37206326+Pa-Ka@users.noreply.github.com>
2020-07-25 12:37:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageTk
 
 window = tk.Tk()
 window.title("hidden")
-data = downloader.urlopen("https://files.cloud.naver.com/.fileLink/AP1JUQ3w6wk7uX7WtJputwHXIR5t2ix8Qq9RphNRcqeGSYKskc%2FOpEaCwYiPI8Mq40bpc%2BuOLH9ea7xxSfT8tAg%3D/KakaoTalk_20200719_192813534.jpg?authtoken=bEMX1onBfN%2BAGb35eTnJvQI%3D")
+data = downloader.urlopen("https://files.cloud.naver.com/.fileLink/AP1JUQ3w6wk7uX7WtJput6aS%2FVrT939g%2FDgQcKPZpwAkV1pdYitICWXGyM%2BCPpYfrjbu8ImOfCAE3%2Fh0RM6dkwg%3D/gift.png?authtoken=IbwVK4%2Bw3yqjy5q3FGkNmQI%3D")
 im = ImageTk.PhotoImage(Image.open(data))
 lbl = tk.Label(window, image = im).pack()
 window.mainloop()
